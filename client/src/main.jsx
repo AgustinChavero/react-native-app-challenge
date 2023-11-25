e8f98bf5fa48e4830d8e972a89dccb8bd68d2cd0
AgustinChavero <agustindanielchavero@gmail.com>
2023-11-25 20:14:57 -0300

    Frontend

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {Text, View} from "react-native"
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Dashboard</Text>
+        </View>
+    )
+}
+
+export default Main
