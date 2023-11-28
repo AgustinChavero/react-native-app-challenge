f29e35c8cde0b0f3459cbf688bb53379982ab71c
AgustinChavero <agustindanielchavero@gmail.com>
2023-11-28 17:22:09 -0300

    Fix listen

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ async function startServer() {
     console.log("Starting the server...");
     await connectDatabase();
     console.log("Server is ready and data has been synchronized.");
-    await app.listen(process.env.PORT || 3000);
+    await app.listen({ port: 3000 }, (err) => {
+      if (err) throw err;
+    });
     console.log(`Server running at ${process.env.PORT || 3000}`);
   } catch (error) {
     console.error("Error starting the server:", error);
