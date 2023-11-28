faa139eb465bdd811a47f2296289c4649e16b3e5
AgustinChavero <agustindanielchavero@gmail.com>
2023-11-28 13:15:28 -0300

    fix 2

--- src/app.js
+++ src/app.js
@@ -9,11 +9,11 @@ const userRoute = require("./modules/user/user-route");
 
 const app = fastify({ logger: true });
 
-app.register(discountRoute);
-app.register(invoiceRoute);
-app.register(productRoute);
-app.register(storeRoute);
-app.register(userRoute);
+app.register(discountRoute, { prefix: "/api" });
+app.register(invoiceRoute, { prefix: "/api" });
+app.register(productRoute, { prefix: "/api" });
+app.register(storeRoute, { prefix: "/api" });
+app.register(userRoute, { prefix: "/api" });
 
 app.get("/", async (request, reply) => {
   return reply.send({
